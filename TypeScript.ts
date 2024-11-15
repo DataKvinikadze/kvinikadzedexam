baa99d02eedd18d333cfd84f493481cbd3af6cc5
DataKvinikadze <datakvinikadze07@gmail.com>
2024-11-15 20:32:23 +0400

    1-

--- TypeScript.ts
+++ TypeScript.ts
@@ -2,33 +2,42 @@
 
 // მაგალითი 1
 
-let name = "Alice";
+let nameaa: string = "Alice";
 
 // მაგალითი 2
 
-let numbers = [1, 2, 3];
+let numbers: number[] = [1, 2, 3];
 
 // მაგალითი 3
 
-function multiply(a, b) {
+function multiply(a: number, b: number): number {
   return a * b;
 }
 
 // მაგალითი 4
 
-const user = { id: 1, name: "Alice" };
+const usere: { id: number; name: string } = { id: 1, name: "Alice" };
 
 // მაგალითი 5
 // რისთვის ვიყენებთ არსებული კოდში = Guest-ს :
 
 function greet(name: string = "Guest") {}
 
-// პასუხი:
+// პასუხი: Guest-ის დაწერით ჩვენ, თუ მაგალთად name არ იარსებებს, მის მაგივრად ფუნქცია მიიღებს ამ string-ს.
 
 // მაგალითი 6
 // დაწერეთ ტიპი რომელიც მოერგება ყველა ქვემოთ ჩამოთვლილ ელემენტს. მაგ: type Config = {
 
-const config1 = {
+type config = {
+  theme?: string;
+  tester?: string;
+  options?: {
+    fontSize?: number;
+    layout?: null;
+  };
+};
+
+const config1: config = {
   theme: "dark",
   options: {
     fontSize: 16,
@@ -36,23 +45,34 @@ const config1 = {
   },
 };
 
-const config2 = {
+const config2: config = {
   theme: "dark",
 };
 
-const config3 = {
+const config3: config = {
   tester: "test",
 };
 
 // მაგალითი 7
 // დაწერეთ ტიპი რომელიც მოერგება ყველა ქვემოთ ჩამოთვლილ ელემენტს. მაგ: type MixedArray =
-const array1 = [42, "hello", { name: "Alice" }];
-const array2 = ["apple", true, { isValid: false }];
-const array3 = [];
+type mixedArray = (
+  | number
+  | boolean
+  | string
+  | { name: string }
+  | { isValid: boolean }
+)[];
+
+const array1: mixedArray = [42, "hello", { name: "Alice" }];
+
+const array2: mixedArray = ["apple", true, { isValid: false }];
+
+const array3: mixedArray = [];
 
 // მაგალითი 8
 // აღწერეთ რისი ტიპიზაცია ხდება არსებულ კოდში წერილობით
 
+// ფუნქციის return-ის მნიშვნელობის
 type Handler = {
   process?: () => string;
   validate?: (input: string) => boolean;
@@ -63,7 +83,24 @@ type Handler = {
 
 // შექმენით ტიპი მონაცემისთივს:
 
-type User = {};
+type User = {
+  id: number;
+  username: string;
+  isAdmin: boolean;
+  profile: {
+    fullName: string;
+    age: number;
+    interests: string[];
+  };
+  settings: {
+    theme: string;
+    notifications: {
+      email: boolean;
+      sms: boolean;
+    };
+  };
+  metadata: undefined;
+};
 
 const user: User = {
   id: 101,
